92fa9f64429cf7ba053241699e6fc621f9f23cd7
capo_greco <thomas.capogreco@gmail.com>
2024-08-28 13:00:18 +1000

    etx/api.js seperation

---
+++ etc/api.js
@@ -0,0 +1,34 @@
+
+export const handle_api = async (req, type) => {
+   const bc = new BroadcastChannel (`program`)
+   const db = await Deno.openKv ()
+   const handler = {
+      listen: () => {
+         const body = new ReadableStream ({
+            start: controller => {
+               controller.enqueue (`data: ES established \n\n`)
+               bc.onmessage = async e => {
+                  if (e.data === `update`) {
+                     const { value: { message } } = await db.get ([ `test` ])
+                     controller.enqueue (`data: ${ message } \n\n`)   
+                  }
+               }
+            },
+            cancel: () => bc.close ()
+         })
+         const stream = body.pipeThrough (new TextEncoderStream ())
+         const headers = new Headers ({
+            "content-type": "text/event-stream",
+            "cache-control": "no-cache",
+         })
+         return new Response (stream, { headers })
+      },
+      update: async () => {
+         const json = await req.json ()
+         await db.set ([ `test` ], json)
+         bc.postMessage (`update`)
+         return new Response ()
+      },
+   }
+   return handler[type] ()   
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts"
+import { handle_api } from "./etc/api.js"
 
 console.clear ()
 
@@ -8,43 +9,12 @@ const handler = async req => {
    const path_array = pathname.slice (1).split (`/`)
 
    if (path_array[0] === `api`) {
-      console.log (`api called`)
-      const bc = new BroadcastChannel (`program`)
-      const db = await Deno.openKv ()
-      const api_handler = {
-         listen: () => {
-            const body = new ReadableStream ({
-               start: controller => {
-                  controller.enqueue (`data: ES established \n\n`)
-                  bc.onmessage = async e => {
-                     if (e.data === `update`) {
-                        const { value: { message } } = await db.get ([ `test` ])
-                        controller.enqueue (`data: ${ message } \n\n`)   
-                     }
-                  }
-               },
-               cancel: () => bc.close ()
-            })
-            const stream = body.pipeThrough (new TextEncoderStream ())
-            const headers = new Headers ({
-               "content-type": "text/event-stream",
-               "cache-control": "no-cache",
-            })
-            return new Response (stream, { headers })
-         },
-         update: async () => {
-            const json = await req.json ()
-            await db.set ([ `test` ], json)
-            bc.postMessage (`update`)
-            return new Response ()
-         },
-      }
-      return api_handler[path_array[1]] ()
+      return handle_api (req, path_array[1])
    }
 
    const fsRoot = path_array[0] === `ctrl` ? "" : "synth"
 
-   return serveDir (req, { fsRoot })
+   return serveDir (req, { fsRoot, quiet: true })
 }
 
 Deno.serve ({ handler })
